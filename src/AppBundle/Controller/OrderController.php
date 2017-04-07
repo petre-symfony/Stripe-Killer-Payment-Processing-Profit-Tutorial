6cc80c9d51d20beb1ab74759a60d98489ea90351
petrero <petrero@gmail.com>
2017-04-07 20:16:01 +0300

    Copy code from branch 6

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class OrderController extends BaseController {
   /**
@@ -25,12 +29,47 @@ class OrderController extends BaseController {
    * @Route("/checkout", name="order_checkout")
    * @Security("is_granted('ROLE_USER')")
    */
-  public function checkoutAction(){
+  public function checkoutAction(Request $request){
     $products = $this->get('shopping_cart')->getProducts();
 
+    if ($request->isMethod('POST')){
+      $token = $request->get('stripeToken');
+      
+      \Stripe\Stripe::setApiKey($this->getParameter('stripe_secret_key'));
+      
+      /** @var User @user */
+      $user = $this->getUser();
+      if (!$user->getStripeCustomerId()){
+        $customer = \Stripe\Customer::create([
+          'email' => $user->getEmail(),
+          'source' => $token  
+        ]);
+        $user->setStripeCustomerId($customer->id);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+      } else {
+        $customer = \Stripe\Customer::retrieve($user->getStripeCustomerId());
+        $customer->source = $token;
+        $customer->save();
+      }
+      
+      \Stripe\Charge::create(array(
+        "amount" => $this->get('shopping_cart')->getTotal() * 100,
+        "currency" => "usd",
+        "customer" => $user->getStripeCustomerId(),
+        "description" => "First test charge!"
+      ));
+      
+      $this->get('shopping_cart')->emptyCart();
+      $this->addFlash('success', 'Order Complete! Yay!');
+      
+      return $this->redirectToRoute('homepage');
+    }
     return $this->render('order/checkout.html.twig', array(
       'products' => $products,
-      'cart' => $this->get('shopping_cart')
+      'cart' => $this->get('shopping_cart'),
+      'stripe_public_key' => $this->getParameter('stripe_public_key') 
     ));
 
   }
