4fd7ed981eab4aea4353aa44c97929c2ce3cf6b4
petrero <petrero@gmail.com>
2017-04-07 21:16:06 +0300

    The only problem is with the entity manager - the code used to update the user record in the database. The way we fix this is a bit specific to Symfony. First, add a public function __construct() with an EntityManager {dollar_sign}em argument. Set this on a new {dollar_sign}em property

--- src/AppBundle/StripeClient.php
+++ src/AppBundle/StripeClient.php
@@ -3,15 +3,23 @@
 namespace AppBundle;
 
 use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
 
 class StripeClient {
+  private $em;
+  
+  public function __construct(EntityManager $em) {
+    $this->em = $em;
+  }
+
+
   public function createCustomer(User $user, $paymentToken){
     $customer = \Stripe\Customer::create([
       'email' => $user->getEmail(),
       'source' => $paymentToken  
     ]);
     $user->setStripeCustomerId($customer->id);
-    $em = $this->getDoctrine()->getManager();
+    $em = $this->em;
     $em->persist($user);
     $em->flush();
     
