979b5f92d1aea26dd21c5c073c40f2d7c078300e
petrero <petrero@gmail.com>
2017-04-07 18:48:11 +0300

    Copy code from branch 5

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -6,6 +6,8 @@ use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class OrderController extends BaseController {
   /**
@@ -25,12 +27,30 @@ class OrderController extends BaseController {
    * @Route("/checkout", name="order_checkout")
    * @Security("is_granted('ROLE_USER')")
    */
-  public function checkoutAction(){
+  public function checkoutAction(Request $request){
     $products = $this->get('shopping_cart')->getProducts();
 
+    if ($request->isMethod('POST')){
+      $token = $request->get('stripeToken');
+      
+      \Stripe\Stripe::setApiKey($this->getParameter('stripe_secret_key'));
+      
+      \Stripe\Charge::create(array(
+        "amount" => $this->get('shopping_cart')->getTotal() * 100,
+        "currency" => "usd",
+        "source" => $token, // obtained with Stripe.js
+        "description" => "First test charge!"
+      ));
+      
+      $this->get('shopping_cart')->emptyCart();
+      $this->addFlash('success', 'Order Complete! Yay!');
+      
+      return $this->redirectToRoute('homepage');
+    }
     return $this->render('order/checkout.html.twig', array(
       'products' => $products,
-      'cart' => $this->get('shopping_cart')
+      'cart' => $this->get('shopping_cart'),
+      'stripe_public_key' => $this->getParameter('stripe_public_key') 
     ));
 
   }
