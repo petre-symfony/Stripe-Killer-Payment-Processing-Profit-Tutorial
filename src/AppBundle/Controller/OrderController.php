18c607bb40f53c76a27bc7ba23d9b12ba53e83fd
petrero <petrero@gmail.com>
2017-04-08 12:06:16 +0300

    Copy code from branch 12

--- src/AppBundle/Controller/OrderController.php
+++ src/AppBundle/Controller/OrderController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\HttpFoundation\Request;
+
+
 
 class OrderController extends BaseController {
   /**
@@ -22,15 +26,43 @@ class OrderController extends BaseController {
   }
 
   /**
-   * @Route("/checkout", name="order_checkout")
+   * @Route("/checkout", name="order_checkout", schemes={"%secure_channel%"})
    * @Security("is_granted('ROLE_USER')")
    */
-  public function checkoutAction(){
+  public function checkoutAction(Request $request){
     $products = $this->get('shopping_cart')->getProducts();
 
+    if ($request->isMethod('POST')){
+      $token = $request->get('stripeToken');
+      
+      /** @var User @user */
+      $user = $this->getUser();
+      $stripeClient = $this->get('stripe_client');
+      if (!$user->getStripeCustomerId()){
+        $stripeClient->createCustomer($user, $token); 
+      } else {
+        $stripeClient->updateCustomerCard($user, $token);
+      }
+      
+      foreach($this->get('shopping_cart')->getProducts() as $product){
+        $stripeClient->createInvoiceItem(
+          $product->getPrice() * 100,
+          $user,
+          $product->getName()      
+        ); 
+      }
+      
+      $stripeClient->createInvoice($user, true);
+      
+      $this->get('shopping_cart')->emptyCart();
+      $this->addFlash('success', 'Order Complete! Yay!');
+      
+      return $this->redirectToRoute('homepage');
+    }
     return $this->render('order/checkout.html.twig', array(
       'products' => $products,
-      'cart' => $this->get('shopping_cart')
+      'cart' => $this->get('shopping_cart'),
+      'stripe_public_key' => $this->getParameter('stripe_public_key') 
     ));
 
   }
